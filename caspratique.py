016ed6b0e8ac4cac6c80f1965ed8fe831b99db2e
Axel-342 <axel.mouisson@gmail.com>
2024-03-25 14:55:28 +0100

    modif py good

--- caspratique.py
+++ caspratique.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('test.csv').dropna()
+df = pd.read_csv('test.csv')
 
-df = df.drop(columns=['Series_reference','Suppressed','UNITS','Magnitude','Subject','Group','Series_title_1','Series_title_3','Series_title_4','Series_title_5'])
+df = df.drop(columns=['Series_reference','Suppressed','Suppressed.1','UNITS','Magnitude','Subject','Group','Series_title_1','Series_title_3','Series_title_4','Series_title_5'])
 
 df.to_csv('fichierfin.csv', index=False)
