bb7af6c28420e26be8c3aa0d4f50dfd4bdf0b7e2
Senzu357 <sebi30320@gmail.com>
2024-03-25 16:03:37 +0100

    filtre des données

--- caspratique.py
+++ caspratique.py
@@ -4,4 +4,10 @@ df = pd.read_csv('test.csv')
 
 df = df.drop(columns=['Series_reference','Suppressed','Suppressed.1','UNITS','Magnitude','Subject','Group','Series_title_1','Series_title_3','Series_title_4','Series_title_5']).dropna()
 
-df.to_csv('fichierfin.csv', index=False)
+filtered_df = df[df['Series_title_2'].str.contains('Credit|Debit|Services', na=False)]
+
+filtered_df.to_csv('fichierfin.csv', index=False)
+
+
+
+
