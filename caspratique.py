5bc973b5b798df55d9e0f819c665f0abae2b9c34
Axel-342 <axel.mouisson@gmail.com>
2024-03-25 14:23:49 +0100

    fichier py commencer un peu vite fait

--- caspratique.py
+++ caspratique.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('test (3).csv').dropna()
 
+df = df(columns=['Series_reference','Suppressed','Suppressed','UNITS','Magnitude','Subject','Group','Series_title_1','Series_title_3','Series_title_4','Series_title_5'])
+
+df.to_csv('fichierfin',index=False)
